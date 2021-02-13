902117e0e6f29bee904658dfb84cf24e205a2098
AJ Villalobos <hi@ajvillalobos.com>
2021-02-13 11:12:55 +0800

    style: fix margin line to stretch across page

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,8 +1,8 @@
 function Heading(props) {
   return (
     <div className="text-2xl font-bold flex items-center text-blue-400 heading">
-      <span>{props.name}</span>
-      <div className="bg-blue-400 w-2/4 ml-4" style={{height: "1px"}}></div>
+      <span className="flex-shrink-0">{props.name}</span>
+      <div className="bg-blue-400 w-full ml-4" style={{ height: "1px" }}></div>
     </div>
   );
 }
