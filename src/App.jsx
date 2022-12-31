7d4c933033a71b11731a87df8a4bf9a3d1356420
AJ Villalobos <hello@ajvillalobos.com>
2022-12-31 21:29:05 +0800

    Add initial information

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import "./App.css";
+import profilePhoto from "./assets/profile.jpeg";
 
 function App() {
-  return <h1 className="text-3xl">AJ Villalobos</h1>;
+  return (
+    <main className="h-screen flex flex-col items-center justify-center space-y-6">
+      <img
+        src={profilePhoto}
+        alt="Profile Photo"
+        className="w-36 h-36 rounded-full"
+      />
+
+      <p className="max-w-md text-center">
+        Hello! I'm AJ, a web developer from the Philippines. I'm still updating
+        this site. Please come back later.
+      </p>
+
+      <p className="max-w-md text-center">
+        For the meantime, you can know more about me on the following places:
+      </p>
+
+      <ul className="flex space-x-4">
+        <li className="underline underline-offset-4">
+          <a href="https://www.linkedin.com/in/aj-villalobos/">LinkedIn</a>
+        </li>
+        <li className="underline underline-offset-4">
+          <a href="https://github.com/iamajvillalobos">Github</a>
+        </li>
+      </ul>
+    </main>
+  );
 }
 
 export default App;
